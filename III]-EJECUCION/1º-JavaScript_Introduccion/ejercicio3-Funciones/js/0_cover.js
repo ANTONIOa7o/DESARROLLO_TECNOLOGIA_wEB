f7f8f9038e7737233a2ea5655839a020160cc562
@ndragogo´s formacion <78384046+ANTONIOa7o@users.noreply.github.com>
2024-03-19 13:24:50 +0100

    Add files via upload

---
+++ III]-EJECUCION/1º-JavaScript_Introduccion/ejercicio3-Funciones/js/0_cover.js
@@ -0,0 +1,12 @@
+swal({
+    title:'Comenzamos la aventura..',
+    text:'Preparado para tu despegue...',
+    button:false,
+    timer:3000,
+}) 
+
+function redireccionar() {
+
+    location.href='https://codepen.io/ANTONIOa7o';
+    
+}
